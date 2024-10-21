b27ab9a2374a4790a689c28d5c0a933aeb60ea3f
user_07 <1.brightNed@gmail.com>
2024-10-21 22:23:53 +0330

    done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import flask;
-from flask import request, redirect, send_file
+from flask import request, redirect, send_file, render_template
 from dataclasses import dataclass 
 from typing import *;
 from time import time_ns;
@@ -14,19 +14,22 @@ class RequsetRecord:
 
 
 
-
+NS_COEF = 10**9;
 BUCKET:Dict[str, List[RequsetRecord]] = dict();
-RATE_DURATION:int = 3600;
-RATE_COUNT_LIMIT = 10;
+RATE_DURATION:int = 5*NS_COEF; #in ns
+RATE_COUNT_LIMIT = 3; #in ns
 
 def add_request_record(ip)->RequsetRecord:
     global BUCKET
     output:RequsetRecord = RequsetRecord(ip=ip, unix=time_ns());
-    BUCKET[ip] = (output);
+    if ip not in BUCKET:
+        BUCKET[ip] = [output,];
+    else:
+        BUCKET[ip].append(output);
     return output;
 
 
-def count_for_ip(ip:str)->int:
+def get_rate_for_ip(ip:str)->int:
     output:int = 0;
     if ip not in BUCKET:
         return output;
@@ -34,12 +37,27 @@ def count_for_ip(ip:str)->int:
     req:RequestRecord;
     current_unix:int = time_ns();
     for req in reqs:
-        output += current_unix - req.unix >= RATE_DURATION;
+        output += (current_unix - req.unix) <= RATE_DURATION;
     return output
+
+def count_for_ip(ip:str)->int:
+    if ip not in BUCKET:
+        return None;
+    return BUCKET[ip].__len__();
 
 
 def is_ip_limited(ip:str)->bool:
-    return count_for_ip(ip=ip) >= RATE_COUNT_LIMIT;
+    count:int = get_rate_for_ip(ip=ip);
+    print(f"""
+[DEBUG]:
+    IP:{ip}
+    visits in last {RATE_DURATION} secs:
+        {count}
+    total requests:
+        {count_for_ip(ip)}
+
+        """)
+    return count >= RATE_COUNT_LIMIT;
 
 
 
@@ -53,16 +71,19 @@ def rate_limit_checker():
     if request.url_rule == "/sorry":
         return None
     ip:str = request.remote_addr;
+
     req : RequestRecord = add_request_record(ip=ip);
     
-    if is_ip_limited:
-        return "you are being rate limited!";
+    if is_ip_limited(ip=ip):
+        return render_template("sorry.html", RATE_DURATION=RATE_DURATION/NS_COEF)
     else:
         return None
 
     
 
 
-@app.route("/test")
+@app.route("/pack_1")
 def hh_test():
-    return send_file("./test.txt");
+    return send_file("pack_1.zip");
+
+
